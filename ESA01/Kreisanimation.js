9a6587386abf5be8348d0990684eee87ae21d345
Ruwimm <110912909+Ruwimm@users.noreply.github.com>
2025-10-06 14:53:14 +0200

    Changed Image with self rendered Image Sequence, added Buttongs and Shortcuts, added Infos

--- ESA01/Kreisanimation.js
+++ ESA01/Kreisanimation.js
@@ -1,5 +1,4 @@
-window.onload = function(){
-
+window.onload = function() {
     const canvas = document.getElementById("glcanvas");
     const gl = canvas.getContext("webgl");
     if (!gl) {
@@ -7,33 +6,81 @@ window.onload = function(){
         return;
     }
 
-    //Hintergrundfarbe
+    // Hintergrundfarbe
     gl.clearColor(0.8, 1, 1, 1.0);
-
-
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    // Bildanimation
+    const imgElement = document.getElementById("meinKreis");
+    let count = 1;
+    let playing = true;
+    let intervalId = null;
 
-
-
-    //Rotierende Kreisanimation Anfang
-    let imgElemet = document.getElementById("meinKreis");
-    let angle = 0;
-
-    function nextImage() {
-        angle += 15;
-
-        if(angle >= 360){
-            angle = 0;
-        }
-
-        imgElemet.src ="RefreshCircle-Sprites/RefreshCircle_" + angle +".png";
-        console.log(angle);
-        console.log(imgElemet);
+    function updateImage() {
+        count++;
+        if (count > 24) count = 1;
+        imgElement.src = `Shuriken/Shuriken${count}.png`;
     }
 
-    setInterval(nextImage, 100);
-    //Rotierende Kreisanimation Anfang
-}
+    function startAnimation() {
+        if (!intervalId) {
+            intervalId = setInterval(updateImage, 100);
+            playing = true;
+        }
+    }
 
+    function stopAnimation() {
+        clearInterval(intervalId);
+        intervalId = null;
+        playing = false;
+    }
 
+    // automatisch starten
+    startAnimation();
+
+    // Tastatursteuerung
+    document.addEventListener("keydown", (event) => {
+        switch (event.key) {
+            case "a":
+                if (playing) stopAnimation();
+                else startAnimation();
+                break;
+            case "r":
+                stopAnimation();
+                count++;
+                if (count > 24) count = 1;
+                imgElement.src = `Shuriken/Shuriken${count}.png`;
+                break;
+            case "i":
+                stopAnimation();
+                count--;
+                if (count < 1) count = 24;
+                imgElement.src = `Shuriken/Shuriken${count}.png`;
+                break;
+        }
+    });
+
+    // Buttonsteuerung
+    const btnStopStart = document.getElementById("btnStopStart");
+    const btnPrev = document.getElementById("btnPrev");
+    const btnNext = document.getElementById("btnNext");
+
+    btnStopStart.addEventListener("click", () => {
+        if (playing) stopAnimation();
+        else startAnimation();
+    });
+
+    btnPrev.addEventListener("click", () => {
+        stopAnimation();
+        count--;
+        if (count < 1) count = 24;
+        imgElement.src = `Shuriken/Shuriken${count}.png`;
+    });
+
+    btnNext.addEventListener("click", () => {
+        stopAnimation();
+        count++;
+        if (count > 24) count = 1;
+        imgElement.src = `Shuriken/Shuriken${count}.png`;
+    });
+};
